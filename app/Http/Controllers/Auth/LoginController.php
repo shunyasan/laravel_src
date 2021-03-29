236a9ca5ce78ead4015d71f27978cfc855e5e25a
shunya <shunya410.coo@icloud.com>
2021-03-29 11:36:40 +0900

    ログアウト推移先

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -27,6 +28,16 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = '/blog';
+
+    /**
+    * ログアウトの推移先
+    *
+    * @return void
+    */
+    protected function loggedOut(Request $request)
+    {
+      return redirect(route('blog.index'));
+    }
 
     /**
      * Create a new controller instance.
